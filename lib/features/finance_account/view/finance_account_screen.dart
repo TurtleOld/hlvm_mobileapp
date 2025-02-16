a9f839847e0d6eb09cf74fbc0d2b73abf9cea63b
Alexander Pavlov <dev@pavlovteam.ru>
2025-02-16 23:31:07 +0300

    refactor: update grandle version

--- lib/features/finance_account/view/finance_account_screen.dart
+++ lib/features/finance_account/view/finance_account_screen.dart
@@ -8,13 +8,22 @@ class FinanceAccountScreen extends StatefulWidget {
 }
 
 class _FinanceAccountScreenState extends State<FinanceAccountScreen> {
+
+  final List<String> _accountList = [
+    "Счет 1",
+    "Счет 2",
+    "Счет 3",
+    "Счет 4",
+    "Счет 5",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Список счетов', style: TextStyle(color: Colors.green),),
-        centerTitle: true,
-      )
+        appBar: AppBar(
+          title: const Text('Список счетов', style: TextStyle(color: Colors.white),),
+          backgroundColor: Colors.green,
+          centerTitle: true,
+        ),
     );
   }
 }
